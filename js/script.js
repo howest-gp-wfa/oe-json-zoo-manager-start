1f93dba83c68a79a590848ca3558fd630e27c9b1
Mileto Di Marco <45310927+Mileto75@users.noreply.github.com>
2022-04-22 17:29:40 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,22 @@
+"use strict"
+
+window.addEventListener('load', initialise);
+
+//global vars
+let mainAnimals;
+const source = 'docs/api/animals.json';
+async function initialise() {
+    mainAnimals = document.querySelector('#animals');
+}
+
+function generateCard(animal){
+    
+}
+
+function selectAnimal(e) {
+    
+}
+
+async function getJsonData(source) {
+   
+}
